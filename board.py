9c09684bcc2a86482de53c1a3c84ef365391fd01
Imran Yafith <imranyafith@gmail.com>
2024-03-18 15:36:52 -0400

    refactor: check_winner and check_draw methods

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 class Board:
     def __init__(self):
+        # TODO: Implement board with 2D list
         self.board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     def print_board(self):
@@ -18,3 +19,27 @@ class Board:
         else:
             self.board[move] = "X" if player == 1 else "O"
             return True
+
+    def check_winner(self):
+        for i in range(0, 3):
+            # Check columns
+            if self.board[i] == self.board[i + 3] == self.board[i + 6]:
+                return True
+            # Check rows
+            if self.board[i * 3] == self.board[i * 3 + 1] == self.board[i * 3 + 2]:
+                return True
+
+        # Check main diagonal
+        if self.board[0] == self.board[4] == self.board[8]:
+            return True
+
+        # Check other diagonal
+        if self.board[2] == self.board[4] == self.board[6]:
+            return True
+        return False
+
+    def check_draw(self):
+        for cell in self.board:
+            if cell != "X" and cell != "O":
+                return False
+        return True
--- game.py
+++ game.py
@@ -9,14 +9,14 @@ class Game:
 
     def start(self):
         self.board.print_board()
-        while not self.check_winner():
-            if self.check_draw():
+        while not self.board.check_winner():
+            if self.board.check_draw():
                 print("It's a draw!")
                 return
             if self.cur_player == 1:
                 self.player_move()
             else:
-                self.player_move()
+                self.computer_move()
             self.board.print_board()
             self.switch_player()
         print(f"{self.player_name} wins!" if self.cur_player == 2 else "Computer wins!")
@@ -32,36 +32,13 @@ class Game:
             move = input(f"{self.player_name}, enter cell number to make your move: ")
             valid_move = self.board.make_move(move, self.cur_player)
 
-    def get_computer_move(self):
+    # TODO: Implement 2-player mode
+    # OR
+    # TODO: Implement better AI
+    def computer_move(self):
         print("Computer's move...")
         move = random.randint(1, 9)
         valid_move = self.board.make_move(move, self.cur_player)
         while not valid_move:
             move = random.randint(1, 9)
             valid_move = self.board.make_move(move, self.cur_player)
-
-    def check_winner(self):
-        for i in range(0, 3):
-            if (
-                self.board.board[i]
-                == self.board.board[i + 3]
-                == self.board.board[i + 6]
-            ):
-                return True
-            if (
-                self.board.board[i * 3]
-                == self.board.board[i * 3 + 1]
-                == self.board.board[i * 3 + 2]
-            ):
-                return True
-        if self.board.board[0] == self.board.board[4] == self.board.board[8]:
-            return True
-        if self.board.board[2] == self.board.board[4] == self.board.board[6]:
-            return True
-        return False
-
-    def check_draw(self):
-        for cell in self.board.board:
-            if cell != "X" and cell != "O":
-                return False
-        return True
